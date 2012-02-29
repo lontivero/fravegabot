b3cc40fbf5adf025bc616c7fe4d7931342a1a842
Lucas Ontivero <lucasontivero@hotmail.com>
2012-02-28 23:32:30 -0300

    debug feature

--- Fravegabot/Controllers/UserMessageProcessorController.cs
+++ Fravegabot/Controllers/UserMessageProcessorController.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Fravegabot.Controllers
@@ -8,7 +12,7 @@ namespace Fravegabot.Controllers
         [AcceptVerbs("GET", "POST")]
         public ActionResult SendMessage(UserMessageModel model)
         {
-            return SendToUser(model.Message);
+            return SendToUser(Request.Dump());
         }
 
         private static ActionResult SendToUser(string message)
@@ -26,5 +30,42 @@ namespace Fravegabot.Controllers
         public string Channel { get; set; }
         public string Message { get; set; }
         public long Step { get; set; }
+    }
+
+    public static class RequestExtensions
+    {
+        public static string Dump(this HttpRequestBase request)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("");
+            sb.AppendLine("-------------  PostedParameters --------------");
+            foreach (var key in request.Form.Keys.Cast<string>().OrderBy(f => f))
+            {
+                sb.AppendLine(key.PadLeft(40, ' ') + " : " + request.Params[key]);
+            }
+
+            sb.AppendLine("");
+            sb.AppendLine("-------------  Query String ------------------");
+            foreach (var key in request.QueryString.Keys.Cast<string>().OrderBy(f => f))
+            {
+                sb.AppendLine(key.PadLeft(40, ' ') + " : " + request.QueryString[key]);
+            }
+
+            sb.AppendLine("");
+            sb.AppendLine("-------------  Http Headers -------------------");
+            foreach (var key in request.Headers.Keys.Cast<string>().OrderBy(f => f))
+            {
+                sb.AppendLine(key.PadLeft(40, ' ') + " : " + request.Headers[key]);
+            }
+
+            sb.AppendLine("");
+            sb.AppendLine("--------------  Cookies -------------------------");
+            foreach (var key in request.Cookies.Keys.Cast<string>().OrderBy(f => f))
+            {
+                sb.AppendLine(key.PadLeft(40, ' ') + " : " + request.Cookies[key].Value + " - ExpireDate: " +  request.Cookies[key].Expires);
+            }
+
+            return sb.ToString();
+        }
     }
 }
